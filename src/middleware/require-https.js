12f7fcf22aad2d52d6dfbe218440750657792588
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-18 13:11:30 +0100

    Update require-https.js
    correction gnarly fixes to logging

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -2,12 +2,16 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
   if (req.secure) {
     // Allow requests only over https
     console.log(req.url);
-    console.log(req.url.split(":")[0]);
+ 
     var request_url_splitted = req.url.split("?")[1];
 
     var request_url_splitted_step_a = request_url_splitted.split("=")[1];
 
     var request_url_splitted_step_b = request_url_splitted_step_a.split(":")[0];
+	  
+    console.log(request_url_splitted_step_a.split(":")[0]);
+	  
+    console.log(request_url_splitted_step_a);
 	
     if (request_url_splitted_step_a.split(":")[0] !== "https") {
 
