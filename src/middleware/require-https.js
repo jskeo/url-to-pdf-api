e3a22ec2e17eed83b67d2bc2da38ef125734bb95
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-18 13:09:14 +0100

    Update require-https.js
    really gnarly quick and dirty fixes to logging and https only

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -3,7 +3,13 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
     // Allow requests only over https
     console.log(req.url);
     console.log(req.url.split(":")[0]);
-    if (req.url.split(":")[0] !== "https") {
+    var request_url_splitted = req.url.split("?")[1];
+
+    var request_url_splitted_step_a = request_url_splitted.split("=")[1];
+
+    var request_url_splitted_step_b = request_url_splitted_step_a.split(":")[0];
+	
+    if (request_url_splitted_step_a.split(":")[0] !== "https") {
 
 		const err = new Error('Only HTTPS allowed.');
 		err.status = 403;
