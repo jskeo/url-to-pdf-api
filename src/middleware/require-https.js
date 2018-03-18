89b3d111ae6916557b23c6bd1f9c34a91cf019f8
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-18 12:54:32 +0100

    Update require-https.js
    fix only https allowed

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -2,6 +2,15 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
   if (req.secure) {
     // Allow requests only over https
     console.log(req.url);
+    if (req.url.split(":")[0] !== "https") {
+
+		const err = new Error('Only HTTPS allowed.');
+		err.status = 403;
+		next(err);
+	}
+
+	else
+    
     return next();
   }
 
