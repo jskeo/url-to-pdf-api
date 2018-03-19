a79ee732d1c0f4374c41820f188a3bf33ea5d219
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-19 03:13:20 +0100

    Update router.js
    gnarly router

--- src/router.js
+++ src/router.js
@@ -26,13 +26,7 @@ function createRouter() {
     logger.warn('Warning: no authentication required to use the API');
   }
  
-   if (router.req.method !== "GET") {
-
-            const err = new Error('Access denied');
-            err.status = 403;
-            next(err);
-        } else {
-
+  
 
   const getRenderSchema = {
     query: renderQuerySchema,
@@ -59,7 +53,7 @@ function createRouter() {
  
   
   return router;
-  }
+  
 }
 
 module.exports = createRouter;
