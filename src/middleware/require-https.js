239e2e32833f33524c10c897d22ee95deff16868
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-20 10:12:00 +0100

    Update require-https.js
    gnarly logging

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -1,5 +1,8 @@
 const createRequireHttps = () => function RequireHttps(req, res, next) {
   console.log(req.protocol);
+  console.log(req.param('protocol'));
+  console.log(req.param('url'));
+  console.log(req.param('x-forwarded-proto'));
         
   if (req.protocol == 'https') {
     // Allow requests only over https
