755606e42575c21f00686b3aa07bfe4be30c2c96
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-20 19:01:23 +0100

    Update require-https.js
    cant be an error

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -3,6 +3,24 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
     console.log('rawHeaders: 	', req.rawHeaders);
     //Accept
     console.log('Accept:   ', req.rawHeaders[9]);
+    //AWS Load Balancer
+    console.log('X-Forwarded-Proto:   ', req.rawHeaders[21]);
+    //AWS Load Balancer
+    console.log('X-Forwarded-Port:  ', req.rawHeaders[23]);
+    //request_url
+    console.log('request_url: 		', req.url);
+    //request_url_length
+    console.log('request_url_length: 	', req.url.length);
+    //request_method should be GET
+    console.log('request_method: 	', req.method);
+    //original_url
+    console.log('originalUrl: 	', req.originalUrl);
+    //parsed_url
+    console.log('_parsedUrl: 	', req._parsedUrl);
+    //params
+    console.log('params' 	,req.params);
+ 	  //query
+    console.log('query 		', req.query);
     
         
   if (req.rawHeaders[21] == 'https') {
