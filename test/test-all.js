d2e8082c9af6ffbcef5dd32518654c0923bbdaf6
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-20 12:09:42 +0100

    Update test-all.js
    revert to old tests

--- test/test-all.js
+++ test/test-all.js
@@ -12,53 +12,43 @@ describe('GET /api/render', () => {
     request(app).get('/api/render').expect(400)
   );
 
-  it('invalid domain should cause an error', () =>
+  it('invalid cert should cause an error', () =>
     request(app)
       .get('/api/render')
       .query({
         url: 'https://self-signed.badssl.com/',
       })
-      .expect(403)
-  );
-  
-  it('valid domain from env-vars should return 200', () =>
-    request(app)
-      .get('/api/render')
-      .query({
-        url: ALLOW_DOMAIN,
-      })
-      .expect(200)
+      .expect(500)
   );
 
-
-  it('bug in original. this version should not take other domains than the allowed one but throw 403. for ref invalid cert should be status 200 when ignoreHttpsErrors=true', () =>
+  it('invalid cert should not cause an error when ignoreHttpsErrors=true', () =>
     request(app)
       .get('/api/render')
       .query({
         url: 'https://self-signed.badssl.com/',
         ignoreHttpsErrors: true,
       })
-      .expect(403)
+      .expect(200)
   );
 });
 
 describe('POST /api/render', () => {
-  it('POST method should not succeed but throw forbidden', () =>
+  it('body must have "url" attribute', () =>
     request(app)
       .post('/api/render')
       .send({
         pdf: { scale: 2 },
       })
       .set('content-type', 'application/json')
-      .expect(403)
+      .expect(400)
   );
 
-  it('render google.com should throw forbidden', () =>
+  it('render google.com should succeed', () =>
     request(app)
       .post('/api/render')
       .send({ url: 'https://google.com' })
       .set('content-type', 'application/json')
-      .expect(403)
+      .expect(200)
       .expect('content-type', 'application/pdf')
       .then((response) => {
         const length = Number(response.headers['content-length']);
@@ -66,12 +56,12 @@ describe('POST /api/render', () => {
       })
   );
 
-  it('html in json body should be forbidden', () =>
+  it('html in json body should succeed', () =>
     request(app)
       .post('/api/render')
       .send({ html: getResource('postmark-receipt.html') })
       .set('content-type', 'application/json')
-      .expect(403)
+      .expect(200)
       .expect('content-type', 'application/pdf')
       .then((response) => {
         const length = Number(response.headers['content-length']);
@@ -79,12 +69,12 @@ describe('POST /api/render', () => {
       })
   );
 
-  it('html as text body should be forbidden', () =>
+  it('html as text body should succeed', () =>
     request(app)
       .post('/api/render')
       .send(getResource('postmark-receipt.html'))
       .set('content-type', 'text/html')
-      .expect(403)
+      .expect(200)
       .expect('content-type', 'application/pdf')
       .then((response) => {
         const length = Number(response.headers['content-length']);
