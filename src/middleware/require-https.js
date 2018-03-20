74bbf9fd062ff81968db3767e101dac05db32979
jskeo <34724276+jskeo@users.noreply.github.com>
2018-03-20 11:28:13 +0100

    Update require-https.js
    syntax fix

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -1,6 +1,6 @@
 const createRequireHttps = () => function RequireHttps(req, res, next) {
   console.log(req.protocol);
-  console.log(req.rawHeaders[]);
+  console.log(req.rawHeaders);
   console.log(req.rawHeaders[23]);
         
   if (req.rawHeaders[23] == 'https') {
