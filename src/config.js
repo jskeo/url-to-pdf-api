9509b04cd4d24714176402006f623fc97e8e6899
jskeo <jerome.standfest@gmail.com>
2018-03-21 12:08:25 +0100

    syntax fix added gnarly missing komma to env vars

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
   ALLOW_HTTP: process.env.ALLOW_HTTP === 'false',
   DEBUG_MODE: process.env.DEBUG_MODE === 'true',
   ALLOW_POST: process.env.ALLOW_POST === 'false',
-  ALLOW_DOMAIN: process.env.ALLOW_DOMAIN
+  ALLOW_DOMAIN: process.env.ALLOW_DOMAIN,
   REQUEST_ARRAY: process.env.REQUEST_ARRAY,
   REQUEST_URL_LENGTH: process.env.REQUEST_URL_LENGTH,
   API_TOKENS: [],
