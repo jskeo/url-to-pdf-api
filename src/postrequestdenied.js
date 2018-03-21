c67d7385fa14d7eaa0ff03dda1e15b9bac1da2f9
jskeo <jerome.standfest@gmail.com>
2018-03-21 13:31:54 +0100

    gnarly post request denied

--- src/postrequestdenied.js
+++ src/postrequestdenied.js
@@ -1,5 +1,5 @@
-const err = new Error('Invalid Request.');
-            err.status = 403;
-            return next(err);
-
-module.exports = postRequestDenied;
+function postRequestDenied {
+	const err = new Error('Invalid Request.');
+    err.status = 403;
+    return next(err);
+}
