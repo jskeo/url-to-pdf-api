ae5ef27f9495456f4b4ef36e3bab50df1f05c383
jskeo <jerome.standfest@gmail.com>
2018-03-21 21:43:50 +0100

    more error handling

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -2,30 +2,96 @@ const config = require('../config');
 
 const createRequireHttps = () => function RequireHttps(req, res, next) {
     //rawHeaders Array
-    console.log('rawHeaders:    ', req.rawHeaders);
-    //Accept
-    console.log('Accept:   ', req.rawHeaders[9]);
-    //AWS Load Balancer
-    console.log('X-Forwarded-Proto:   ', req.rawHeaders[21]);
-    //AWS Load Balancer
-    console.log('X-Forwarded-Port:  ', req.rawHeaders[23]);
-    //request_url
-    console.log('request_url:       ', req.url);
-    //request_url_length
-    console.log('request_url_length:    ', req.url.length);
-    //request_method should be GET
-    console.log('request_method:    ', req.method);
-    //original_url
-    console.log('originalUrl:   ', req.originalUrl);
-    //parsed_url
-    console.log('_parsedUrl:    ', req._parsedUrl);
-    //params
-    console.log('params', req.params);
-    //query
-    console.log('query      ', req.query);
-    //undefinded
     try {
-    console.log('req.url_undef:         ', isEmpty(req.url.split("?")[1].split("=")[1].split(":")[1].split("//")[2].split("/")[4]));
+    console.log('rawHeaders:    ', req.rawHeaders);
+    catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //Accept
+    try {
+    console.log('Accept:   ', req.rawHeaders[9]);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //AWS Load Balancer
+    try {
+    console.log('X-Forwarded-Proto:   ', req.rawHeaders[21]);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //AWS Load Balancer
+    try {
+    console.log('X-Forwarded-Port:  ', req.rawHeaders[23]);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //request_url
+    try {
+    console.log('request_url:       ', req.url);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //request_url_length
+    try {
+    console.log('request_url_length:    ', req.url.length);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //request_method should be GET
+    try {
+    console.log('request_method:    ', req.method);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //original_url
+    try {
+    console.log('originalUrl:   ', req.originalUrl);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //parsed_url
+    try {
+    console.log('_parsedUrl:    ', req._parsedUrl);
+    } catch(error) {
+        const err = new Error('Invalid Request.');
+                err.status = 403;
+                return next(err);
+    }
+    //params
+    try {
+    console.log('params', req.params);
+    } catch(error) {
+        const err = new Error('Invalid Request Params.');
+                err.status = 403;
+                return next(err);
+    }
+    //query
+    try {
+    console.log('query      ', req.query);
+    } catch(error) {
+        const err = new Error('Invalid Request Query.');
+                err.status = 403;
+                return next(err);
+    }
+    //requestId
+    try {
+    console.log('requestId:         ', isEmpty(req.url.split("?")[1].split("=")[1].split(":")[1].split("//")[2].split("/")[3]));
     } catch(error) {
         const err = new Error('Invalid Request. 5');
                 err.status = 403;
@@ -39,11 +105,18 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
     // Allow requests only over https
     if (req.url.split("?")[1].split("=")[1].split(":")[0] === 'https') {
     // Cut request into pieces
+    try {
         var target = req.url.split("?")[1].split("=")[1].split(":")[1].split("/")[2];
         var requestUrl = req.url;
         var requestUrlLength = requestUrl.length;
         var requestId = req.url.split("?")[1].split("=")[1].split(":")[1].split("//")[2].split("/")[3];
         var requestIdLength = requestId.length;
+        } catch(error) {
+        const err = new Error('Invalid Request. 6');
+                err.status = 403;
+                return next(err);
+    }
+
     // Logging my last resot
         console.log(target);
         console.log(requestUrlLength);
@@ -59,11 +132,11 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
                 err.status = 403;
                 return next(err);
             } else if (config.REQUEST_URL_LENGTH != requestUrlLength || config.REQUEST_URL_LENGTH_WITHOUT_FLAG != requestUrlLength) {
-                const err = new Error('Invalid Request.');
+                const err = new Error('Invalid Request. 3');
                 err.status = 403;
                 return next(err);
             } else if (config.REQUEST_ID_LENGTH != requestIdLength) {
-                const err = new Error('Invalid Request.');
+                const err = new Error('Invalid Request. 4');
                 err.status = 403;
                 return next(err); 
             }
