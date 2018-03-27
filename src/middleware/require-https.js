da822c1caa17be2309e6d9206bbb02f8145292d2
jskeo <jerome.standfest@gmail.com>
2018-03-27 11:06:19 +0200

    api path condition

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -113,7 +113,7 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
         console.log(check_path)
         if (requestApiPath == check_path) {
             console.log('requestPathToApi OK');
-        } else if (requestApiPath = '/favicon.ico') {
+        } else if (requestApiPath == '/favicon.ico') {
             try {
                 console.log(requestApiPath);
                 console.log(requestApiPath.legth);
