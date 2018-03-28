a31628bfce703e0c6e656f5b1cb4bb84ca51a1be
jskeo <jerome.standfest@gmail.com>
2018-03-28 23:04:33 +0200

    double browser close rewind

--- src/core/pdf-core.js
+++ src/core/pdf-core.js
@@ -100,8 +100,6 @@ async function render(_opts = {}) {
             return next(err);
           }
       ).pdf(opts.pdf);
-    logger.info('Closing browser..');
-    await browser.close();
   } catch (err) {
     logger.error(`Error when rendering page: ${err}`);
     logger.error(err.stack);
