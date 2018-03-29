d7cb1a153d7fd83a6de17edea2e8585875b5b4f7
jskeo <jerome.standfest@gmail.com>
2018-03-29 11:45:07 +0200

    cache II

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 
 // Env vars should be casted to correct types
 const config = {
- // PORT: Number(process.env.PORT) || 9000,
-  PORT: 6379,
+  // PORT: 6379,
+  PORT: Number(process.env.PORT) || 9000,
   NODE_ENV: process.env.NODE_ENV,
   LOG_LEVEL: process.env.LOG_LEVEL,
   ALLOW_HTTP: process.env.ALLOW_HTTP === 'false',
