c554ab1d6f08c7f10df41946c3031a7906284973
jskeo <jerome.standfest@gmail.com>
2018-03-31 12:42:42 +0200

    moms spagetti middleware saves dir

--- src/middleware/require-https.js
+++ src/middleware/require-https.js
@@ -287,7 +287,7 @@ const createRequireHttps = () => function RequireHttps(req, res, next) {
 
                             wget({
                                 url:  req_url,
-                                dest: './',      // destination path or path with filenname, default is ./ 
+                                dest: './src/saves',      // destination path or path with filenname, default is ./ 
                                 timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
                                   },
                                   function (error, response, wgetBody) {
