2c69cb97b34b0e40ec037a035fdf7b5e57550b18
jskeo <jerome.standfest@gmail.com>
2018-03-31 23:53:23 +0200

    break

--- src/middleware/check-routes.js
+++ src/middleware/check-routes.js
@@ -17,10 +17,10 @@ const createCheckRoutes = () => function checkRoutes(req, res, next) {
     logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
 	
     res.status(200).send('CHECK ROUTES ACTIVE I');
-
-    const err = new Error('CHECK ROUTES ACTIVE I');
-    err.status = 200;
-    return next(err);
+    break;
+    // const err = new Error('CHECK ROUTES ACTIVE I');
+    // err.status = 200;
+    // return next(err);
 
 
 	//return next();
