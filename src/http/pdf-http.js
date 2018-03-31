64e17b967e5c083e6f8429907606aa022e982233
jskeo <jerome.standfest@gmail.com>
2018-03-31 21:04:36 +0200

    render query opts handler syntax

--- src/http/pdf-http.js
+++ src/http/pdf-http.js
@@ -86,7 +86,7 @@ function getOptsFromQuery(query) {
       networkIdleTimeout: query['goto.networkIdleTimeout'],
     },
     pdf: {
-      path: req.query.pdf.path,
+      path: query.pdf.path,
       scale: query['pdf.scale'],
       displayHeaderFooter: query['pdf.displayHeaderFooter'],
       landscape: query['pdf.landscape'],
