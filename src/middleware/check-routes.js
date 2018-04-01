e736a1e59d15f84264a4116e39c223845483ad26
jskeo <jerome.standfest@gmail.com>
2018-04-01 20:25:20 +0200

    response end problem

--- src/middleware/check-routes.js
+++ src/middleware/check-routes.js
@@ -81,14 +81,14 @@ const createCheckRoutes = () => function checkRoutes(req, res, next) {
 		    logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
 			console.log('res: ', res._header);
 			response.end();
-		}
+		} else {
 
 		res.send('CHECK ROUTES ACTIVE III');
 
 		logger.info(`X-Forwarded-For: ${req.get('X-Forwarded-For')} .. `);
 		logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
 		console.log('res: ', res._header);
-
+		}
 		//
 		//console.log('Checking if dummy directory exists');
 		//console.log(fs.statSync(process.cwd()+config.SAVES_PATH).isDirectory());
