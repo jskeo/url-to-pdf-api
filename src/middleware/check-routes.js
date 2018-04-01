e60f7ac6f490e869625cff199437e77e8be906cb
jskeo <jerome.standfest@gmail.com>
2018-04-01 20:17:52 +0200

    isDirectory func true response end

--- src/middleware/check-routes.js
+++ src/middleware/check-routes.js
@@ -80,6 +80,7 @@ const createCheckRoutes = () => function checkRoutes(req, res, next) {
 		    logger.info(`X-Forwarded-For: ${req.get('X-Forwarded-For')} .. `);
 		    logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
 			console.log('res: ', res._header);
+			response.end();
 		}
 
 		res.send('CHECK ROUTES ACTIVE III');
