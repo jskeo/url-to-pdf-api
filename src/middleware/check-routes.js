c09c6e3c975f6baf6549ace4b62c2eaea673b6bc
jskeo <jerome.standfest@gmail.com>
2018-04-01 19:49:09 +0200

    await create directory for request id sync

--- src/middleware/check-routes.js
+++ src/middleware/check-routes.js
@@ -24,11 +24,16 @@ function doesDirectoryExist(directoryPath) {
 };
 
 function createDirectory(directoryPath) {    
-	mkdirp(directoryPath, function (err) {
-	    if (err) console.error(err)
-	    else console.log('pow! directory created:', directoryPath)
-	});
+	try {
+	mkdirp(directoryPath);
+	console.log('pow! directory created:', directoryPath);
+	} catch (e) {
+		const err = new Error('Internal Error IV');
+		err.status = 500;
+		return next(err);
+	}
 };
+
 
 const createCheckRoutes = () => function checkRoutes(req, res, next) {
 
@@ -41,24 +46,36 @@ const createCheckRoutes = () => function checkRoutes(req, res, next) {
 	console.log('requestId: ', requestId);
 	//const objectPath = '../saves/'+requestId;
 	const objectPath = process.cwd()+config.SAVES_PATH+"/"+requestId+"/"+config.FILE_NAME;
+	const objectDir = process.cwd()+config.SAVES_PATH+"/"+requestId;
 	console.log('CWD: ', process.cwd());
 
 	if (fs.existsSync(objectPath)) {
 		console.log(objectPath);
     	console.log('file does exist');
+    	await res.status(200).send('CHECK ROUTES ACTIVE I');
+
+	    await logger.info(`X-Forwarded-For: ${req.get('X-Forwarded-For')} .. `);
+	    await logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
+		await console.log('res: ', res._header);
+	
 	} else {
 		console.log(objectPath);
 		console.log('file does not exist');
 		console.log('Checking if directory exists');
 		//
 		//
-		console.log(doesDirectoryExist(objectPath));
+		console.log(doesDirectoryExist(objectDir));
 		//fs.statSync(process.cwd()+config.SAVES_PATH+"/"+requestId).isDirectory();
-		if (!doesDirectoryExist(objectPath)) {
+		if (!doesDirectoryExist(objectDir)) {
 			console.log('Creating directory..');
-			await createDirectory(objectPath);
+			await createDirectory(objectDir);
 			console.log('Checking again if directory exists..');
 			console.log(doesDirectoryExist(objectPath));
+			await res.status(200).send('CHECK ROUTES ACTIVE II');
+
+		    await logger.info(`X-Forwarded-For: ${req.get('X-Forwarded-For')} .. `);
+		    await logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
+			await console.log('res: ', res._header);
 		}
 		//
 		console.log('Checking if dummy directory exists');
@@ -81,12 +98,7 @@ const createCheckRoutes = () => function checkRoutes(req, res, next) {
     //res.set('Cache-Control', 'public, max-age=10800000'); // 34560 30 seconds, 345600 4 days, 10800000 3h
     //res.set('Expires', new Date(Date.now() + 10800000).toUTCString());
 
-    res.status(200).send('CHECK ROUTES ACTIVE I');
-
-    logger.info(`X-Forwarded-For: ${req.get('X-Forwarded-For')} .. `);
-    logger.info(`Status Code: ${res.statusCode} | Status Message ${res.statusMessage} | Response time ${res.get('X-Response-Time')} ..`);
-	console.log('res: ', res._header);
-	
+   
 
     // const err = new Error('CHECK ROUTES ACTIVE I');
     // err.status = 200;
