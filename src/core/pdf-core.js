f14d438cdbfde72402c139df1a9413dc93e9caf9
jskeo <jerome.standfest@gmail.com>
2018-04-04 04:21:28 +0200

    catch all errors

--- src/core/pdf-core.js
+++ src/core/pdf-core.js
@@ -78,10 +78,15 @@ async function render(_opts = {}) {
       logger.info(`Goto url ${opts.url} ..`);
       await page.goto(opts.url, opts.goto);
       const response = await page.goto(opts.url, opts.goto);
+      try {
       console.log(response.status);
        if (response.status != 200) {
          const err = new Error('Invalid Request XII');
          err.status = 403;
+         return err;
+       } } catch (err) {
+         const err = new Error('Invalid Request XIII');
+         err.status = 500;
          return err;
        }
       //logger.info(`Goto url ${opts.url} ..`);
